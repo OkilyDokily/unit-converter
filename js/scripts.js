9bc7df8f6aafd2965c7f637e48b1ded6cc143006
me <me@me.me.com>
2020-03-23 14:22:13 -0700

    create file structure and add 4 unit conversion functions

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var quartsToCups = function(quarts){
+  return quarts * 4;
+}
+
+var gallonsToQuarts = function(gallons){
+  return gallons * 4;
+}
+
+var gallonsToPints = function(gallons){
+  return gallons * 8;
+}
+
+pintsToCups = function(pints){
+  return pints * 2;
+}
+
