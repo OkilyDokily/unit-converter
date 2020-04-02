dd3d194019e2454342698ecf7623a7b85b4768e3
me <me@me.me.com>
2020-04-02 14:31:49 -0700

    refactor to a single foreach array

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,91 @@
-var quartsToCups = function(quarts){
+var quartstocups = function(quarts){
   return quarts * 4;
 }
 
-var gallonsToQuarts = function(gallons){
+var gallonstoquarts = function(gallons){
   return gallons * 4;
 }
 
-var gallonsToPints = function(gallons){
+var gallonstopints = function(gallons){
   return gallons * 8;
 }
 
-var pintsToCups = function(pints){
+var pintstocups = function(pints){
   return pints * 2;
 }
 
-var gallonsToLiters = function(gallons){
+var gallonstoliters = function(gallons){
   return gallons * 3.78541;
 }
 
-var tableSpoonsToTeaspoons = function (tablespoons){
+var tablespoonstoteaspoons = function (tablespoons){
   return tablespoons * 3;
 }
 
-var cupsToTableSpoons = function(cups) {
+var cupstotabletpoons = function(cups) {
   return cups * 16;
 }
 
-var milliliterToTeaspoons = function(milliliters) {
+var milliliterstoteaspoons = function(milliliters) {
   return milliliters * 0.202884;
 }
 
 
 $(document).ready(function(){
-  $()
+
+  var myarray = ["quartstocups","gallonstoquarts", "gallonstopints","pintstocups","gallonstoliters","tablespoonstoteaspoons","cupstotablespoons","milliliterstoteaspoons"];
+  
+  
+  function generate_handler( item ) {
+    return function(e) { 
+     
+      e.preventDefault();
+      var result = window[item]($("#"+item+"input").val());
+      
+      $("#"+item+"result").text(result);
+    };
+  }
 
 
-})
+  myarray.forEach(function(item){
+    console.log("#"+item)
+    $("#"+item).submit(generate_handler(item));
+  });
+
+});
+  // $("#quartstocups").submit(function(e){
+  //   e.preventDefault();
+  //   var result = quartsToCups($("#quartstocupsinput").val());
+  //   $("#quartstocupsresult").text(result);
+  // });
+
+  // $("#gallonstoquarts").submit(function(e){
+  //   e.preventDefault();
+  //   var result = gallonsToQuarts($("#gallonstoquartsinput").val());
+  //   $("#gallonstoquartsresult").text(result);
+  // });
+
+  // $("#gallonstopints").submit(function(e){
+  //   e.preventDefault();
+  //   var result = gallonsToPints($("#gallonstopintsinput").val());
+  //   $("#gallonstopintsresult").text(result);
+  // });
+
+  // $("#pintstocups").submit(function(e){
+  //   e.preventDefault();
+  //   var result = pintsToCups($("#pintstocupinput").val());
+  //   $("#pintstocupsresult").text(result);
+  // });
+
+  // $("#gallonstoliters").submit(function(e){
+  //   e.preventDefault();
+  //   var result = gallonstoliters($("#gallonstolitersinput").val());
+  //   $("#quartstocupsresult").text(result);
+  // });
+
+  // $("#tablespoonstoteaspoons").submit(function(e){
+  //   e.preventDefault();
+  //   var result = tableSpoonsToTeaspoons($("#tablespoonstoteaspoonsinput").val());
+  //   $("#tablespoonstoteaspoonsresult").text(result);
+  // });
+
